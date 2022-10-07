251229ea8a04c4ea0661c9356b10a3346f73c60d
Alexandre <97739767+Crowny23@users.noreply.github.com>
2022-10-07 10:25:09 +0200

    sidebar

--- src/Security/AppUsersAuthAuthenticator.php
+++ src/Security/AppUsersAuthAuthenticator.php
@@ -47,7 +47,7 @@ class AppUsersAuthAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_worksites_index'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
