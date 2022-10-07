64c7ca80e16be14914f666fefd802d57c97a2222
XhesikaMilaqi <97739823+XhesikaMilaqi@users.noreply.github.com>
2022-10-07 15:49:14 +0200

    Add: Form Styling

--- src/Form/CustomersType.php
+++ src/Form/CustomersType.php
@@ -7,21 +7,25 @@ use App\Repository\UsersRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class CustomersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('Town')
-            ->add('postalcode')
-            ->add('address')
-            ->add('mail')
-            ->add('phone')
-            ->add('social_reason')
-            ->add('customer_note')
+            ->add('firstname', TextType::class, ['label' => 'Prenom'])
+            ->add('lastname', TextType::class, ['label' => 'Nom'])
+            ->add('Town', TextType::class, ['label' => 'Ville'])
+            ->add('postalcode', NumberType::class, ['label' => 'Code Postale'])
+            ->add('address', TextType::class, ['label' => 'Addresse'])
+            ->add('mail', EmailType::class, ['label' => 'E-mail'])
+            ->add('phone', TelType::class, ['label' => 'Téléphone'])
+            ->add('social_reason', TextType::class, ['label' => 'Raison Sociale'])
+            ->add('customer_note', TextType::class, ['label' => 'Note'])
         ;
     }
 
