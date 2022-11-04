149a21f69bd5670086d7fa8e84b73c8be9d4a83b
Alexandre <97739767+Crowny23@users.noreply.github.com>
2022-11-04 09:11:59 +0100

    fix datetime worksite

--- src/Form/WorksitesType.php
+++ src/Form/WorksitesType.php
@@ -6,7 +6,6 @@ use App\Entity\Customers;
 use App\Entity\Quotation;
 use App\Entity\WorksiteCategories;
 use App\Entity\Worksites;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
